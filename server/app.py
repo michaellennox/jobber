9784f70576e116a125f866a00192a69ac0811615
Andrew Burnie <apburnie@hotmail.co.uk>
2016-02-09 17:10:04 +0000

    sets up controllers

--- server/app.py
+++ server/app.py
@@ -1,9 +1,12 @@
 from flask import Flask, send_from_directory
 from models.database import db
+# from blueprints.companies_blueprint import companies_page
 import os
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+# app.register_blueprint(companies_page, url_prefix='/companies')
+
 
 from models.companies import Company
 
@@ -15,6 +18,18 @@ def serve_client():
     angular_index = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                                  "..", "public", "views")
     return send_from_directory(angular_index, 'index.html')
+
+@app.route('/companies')
+def get_companies():
+    return "Test Message"
+
+@app.route('/companies/:name')
+def get_specific_company():
+    return "Test Message"
+
+@app.route('/companies', methods = ['POST'])
+def post_companies():
+    return "Test Message"
 
 if __name__ == '__main__':
     app.run(port=8080, debug=True)
