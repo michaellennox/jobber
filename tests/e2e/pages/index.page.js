7f0c9ff8388f79b200fdd01d845f56df79ad505f
Adam Lancaster <lancaster28@hotmail.com>
2016-02-09 11:24:20 +0000

    Adds testing scaffold for Angular

---
+++ tests/e2e/pages/index.page.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var IndexPage = function(){
+
+  this.header = element(by.className("title"));
+
+}
+
+
+IndexPage.prototype.get = function(){
+  browser.get("http://localhost:8080");
+}
+
+module.exports = IndexPage;
