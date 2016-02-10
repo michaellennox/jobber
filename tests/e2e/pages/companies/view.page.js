66367ea6f3c38644b8fedc1b6663d28fc92f6b80
Adam Lancaster <lancaster28@hotmail.com>
2016-02-10 16:57:44 +0000

    Fixes merge conflict

--- tests/e2e/pages/companies/view.page.js
+++ tests/e2e/pages/companies/view.page.js
@@ -1,10 +1,8 @@
 var ViewPage = function() {
-<<<<<<< HEAD
   this.companyName = element(by.binding("ctrl.company.name"));
   this.addPersonLink = element(by.css(".addPerson"));
   this.peopleList = element(by.repeater("person in pCtrl.people"));
   this.person = this.peopleList.first();
-
 };
 
 ViewPage.prototype.viewCompany = function(company){
@@ -13,9 +11,6 @@ ViewPage.prototype.viewCompany = function(company){
 
 ViewPage.prototype.clickAddPersonLink = function(){
   this.addPersonLink.click();
-=======
-  this.companyName = element(by.css("#companyName"));
->>>>>>> a1a34368768345a0409f85763fca201443365299
 };
 
 module.exports = ViewPage;
