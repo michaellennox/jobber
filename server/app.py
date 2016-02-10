b51cbef5136598325614eaeaba22432fb1800af2
James Borrell <james.borrell92@gmail.com>
2016-02-10 12:12:57 +0000

    Get & Post request routes working

--- server/app.py
+++ server/app.py
@@ -1,6 +1,19 @@
 from flask import Flask, send_from_directory, jsonify, request
 from models.database import db
+from sqlalchemy.ext.declarative import declarative_base
 import os
+
+Base = declarative_base()
+
+from sqlalchemy import create_engine
+engine = create_engine('postgresql:///jobber_dev')
+
+from sqlalchemy.orm import sessionmaker
+session = sessionmaker()
+
+session.configure(bind=engine)
+
+Base.metadata.create_all(engine)
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
@@ -27,24 +40,27 @@ def get_companies():
         ])
     return jsonify(companies=list)
 
-@app.route('/api/companies/:name')
-def get_specific_company():
-    name = request.args.get('name')
-    company = Company.query.filter_by(name=name)
+@app.route('/api/companies/<name>')
+def get_specific_company(name):
+    get_company = session()
+    company = get_company.query(Company).filter(Company.name == name).one()
     list = [
     {'param': 'id', 'val': company.id},
     {'param': 'name', 'val': company.name}
     ]
     return jsonify(company=list)
 
-@app.route('/api/companies', methods = ['POST'])
+@app.route('/api/companies/new', methods = ['POST'])
 def post_companies():
     name = request.args.get('name')
-    company = Company(name)
-    if db.session.add(company):
-        return 'Company created'
+    if name:
+        new_company = session()
+        company = Company(name=name)
+        new_company.add(company)
+        new_company.commit()
+        return jsonify(message='Company created')
     else:
-        return 'No'
+        return jsonify(message='No')
 
 if __name__ == '__main__':
     app.run(port=8080, debug=True)
