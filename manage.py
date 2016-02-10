e8abc2219e35843ea66033061b7a30b9c14bd505
michaellennox <michaelctlennox@gmail.com>
2016-02-10 18:45:53 +0000

    Extracts controller and routing logic

--- manage.py
+++ manage.py
@@ -1,14 +1,14 @@
-from flask.ext.script import Manager
+from flask.ext.script import Server, Manager
 from flask.ext.migrate import Migrate, MigrateCommand
 import os
 
-from app import app, db
-app.config.from_object(os.environ['APP_SETTINGS'])
+from server import app, db
 
 migrate = Migrate(app, db, directory='server/migrations')
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
+manager.add_command("runserver", Server(port=8080))
 
 if __name__ == '__main__':
     manager.run()
