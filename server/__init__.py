b3488244edf654b3ac75675affeb0d72a5fb2602
michaellennox <michaelctlennox@gmail.com>
2016-02-11 13:23:36 +0000

    Adds catch all routing logic

--- server/__init__.py
+++ server/__init__.py
@@ -22,11 +22,11 @@ app.config.from_object('server.' + os.environ['APP_SETTINGS'])
 api = Api(app)
 db = SQLAlchemy(app)
 
+import server.controllers.companies
 
-@app.route('/')
-def serve_client():
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve_client(path):
     angular = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                            "..", "public", "views")
     return send_from_directory(angular, 'layout.html')
-
-import server.controllers.companies
