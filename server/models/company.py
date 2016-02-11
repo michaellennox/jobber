764a90972ac39ef3df30870ba760a2b0be7a213e
michaellennox <michaelctlennox@gmail.com>
2016-02-11 15:46:50 +0000

    Adds DB Model for Job

--- server/models/company.py
+++ server/models/company.py
@@ -3,7 +3,7 @@ from server import db
 
 class Company(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String())
+    name = db.Column(db.String(80))
     people = db.relationship("Person", backref="company")
 
     def __init__(self, name):
