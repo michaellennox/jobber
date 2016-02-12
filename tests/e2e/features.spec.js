398a1c1ab20d146410764c3f9a05dc6e1529f2c3
James Borrell <james.borrell92@gmail.com>
2016-02-12 11:56:26 +0000

    Adding & views a new job added to protractor tests, all passing (database reset still necessary)

--- tests/e2e/features.spec.js
+++ tests/e2e/features.spec.js
@@ -3,16 +3,18 @@ describe("Company partials.", function(){
       newPage = require("./pages/companies/new.page.js"),
       viewPage = require("./pages/companies/view.page.js"),
       personNewPage = require("./pages/people/new.page.js"),
+      jobNewPage = require("./pages/jobs/new.job.js")
       companiesNewPage = new newPage,
       companiesViewPage = new viewPage,
       companiesIndexPage = new indexPage,
       peopleNewPage = new personNewPage;
+      jobsNewPage = new jobNewPage;
 
   it("Runs the feature test", function(){
-// VISITS COMPANIES INDEX PAGE  
+// VISITS COMPANIES INDEX PAGE
     companiesIndexPage.get();
 // EXPECTS THERE TO BE NO COMPANIES YET
-    // expect(companiesIndexPage.companiesList.count()).toEqual(0);
+    expect(companiesIndexPage.companiesList.count()).toEqual(0);
 // VISITS ADD COMPANY PAGE AND ADDS A COMPANY
     companiesIndexPage.clickAdd();
     companiesNewPage.addCompany("testCo");
@@ -26,6 +28,13 @@ describe("Company partials.", function(){
     companiesViewPage.clickAddPersonLink();
     peopleNewPage.addPerson("Testla");
 // EXPECTS PERSON TO BE ADDED AND DISPLAYED
-     expect(peopleNewPage.person).toEqual("Testla");
+    expect(companiesViewPage.person.getText()).toEqual("Testla");
+// EXPECTS NO JOBS TO BEGIN WITH
+    expect(companiesViewPage.jobList.count()).toEqual(0)
+// VISITS ADD JOBS PAGE AND ADDS A JOBS
+    companiesViewPage.clickAddJobLink();
+    jobsNewPage.addJob("Developer")
+// EXPECTS JOB TO BE ADDED AND DISPLAYED
+    expect(companiesViewPage.job.getText()).toEqual("Developer")
   });
 });
--- tests/e2e/pages/companies/view.page.js
+++ tests/e2e/pages/companies/view.page.js
@@ -1,14 +1,21 @@
 var ViewPage = function() {
   this.companyName = element(by.binding("ctrl.company.name"));
   this.addPersonLink = element(by.css("#addPerson"));
+  this.addJobLink = element(by.css("#addJob"));
   this.peopleList = element.all(by.repeater("person in ctrl.people"));
   this.person = this.peopleList.get(0);
+  this.jobList = element.all(by.repeater("job in ctrl.jobs"));
+  this.job = this.jobList.get(0);
 };
 
 
 ViewPage.prototype.clickAddPersonLink = function(){
   this.addPersonLink.click();
 };
+
+ViewPage.prototype.clickAddJobLink = function(){
+  this.addJobLink.click();
+}
 
 ViewPage.prototype.get = function(company_id) {
   browser.get("http://localhost:8080/companies/"+company_id)
