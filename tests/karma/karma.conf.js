089770afeb1fff6b8028be4491d723fa79c4aab4
michaellennox <michaelctlennox@gmail.com>
2016-02-14 12:35:58 +0000

    Updates preprocessor path

--- tests/karma/karma.conf.js
+++ tests/karma/karma.conf.js
@@ -32,7 +32,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      '../public/js/**/*.js': ['coverage']
+      '../../public/js/**/*.js': ['coverage']
     },
 
 
