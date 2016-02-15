6ac098c09c94920b1c62cee508aedae80f15c0a7
Hamid Zade <hamidmamadzade@gmail.com>
2016-02-15 20:18:14 +0000

    person web scraper added

--- server/controllers/web_scraper.py
+++ server/controllers/web_scraper.py
@@ -15,6 +15,17 @@ class WebScraperAPI(Resource):
         driver = webdriver.Firefox()
         driver.wait = WebDriverWait(driver, 5)
         driver.get(url)
-        return{'name': driver.find_element_by_id('name').text}
+        return {'name': driver.find_element_by_id('name').text,
+                'summary': driver.find_element_by_class_name('description').text,
+                'job_title': driver.find_element_by_class_name('headline').text,
+                'location': driver.find_element_by_class_name('locality').text,
+                'photo': driver.find_element_by_class_name('image').get_attribute('src')
+        }
         time.sleep(5)
         driver.quit()
+
+# "//img[contains(@src)]/parent::a"
+
+# login_form = driver.find_element_by_xpath("/html/body/form[1]")
+
+# driver.find_element_by_css_selector()
