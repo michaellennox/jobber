6495ec34cf031cd013336066216be7133e97f364
Adam Lancaster <lancaster28@hotmail.com>
2016-02-15 23:31:40 +0000

    Adds form fields to partials/jobs/new.html Adds update to jobs model

--- server/models/job.py
+++ server/models/job.py
@@ -5,6 +5,10 @@ class Job(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(250))
     company_id = db.Column(db.Integer, db.ForeignKey("company.id"))
+    salary = db.Column(db.Integer)
+    location = db.Column(db.String)
+    summary = db.Column(db.String)
+    perks = db.Column(db.String)
 
     def __init__(self, args):
         self.title = args['title']
