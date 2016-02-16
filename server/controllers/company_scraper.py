559d8db1b531b5effbc5e35136bc22b202a68ee6
Hamid Zade <hamidmamadzade@gmail.com>
2016-02-16 14:46:24 +0000

    adds error messages to scrapers

--- server/controllers/company_scraper.py
+++ server/controllers/company_scraper.py
@@ -13,15 +13,21 @@ class CompanyScraperAPI(Resource):
         url = request.json.get('url')
         driver = webdriver.PhantomJS()
         driver.wait = WebDriverWait(driver, 5)
-        driver.get(url)
-        return {
-            'name': driver.find_element_by_class_name('name').text,
-            'summary': driver.find_element_by_class_name('basic-info-description').text,
-            'photo': driver.find_element_by_class_name('image').get_attribute('src'),
-            'website': driver.find_element_by_class_name('website').find_element_by_css_selector('p').text,
-            'industry': driver.find_element_by_class_name('industry').text,
-            'address': driver.find_element_by_class_name('adr').text,
-            'specialties': driver.find_element_by_class_name('specialties').find_element_by_css_selector('p').text
-        }
-        time.sleep(5)
-        driver.quit()
+        try:
+            driver.get(url)
+            results = {
+                'name': driver.find_element_by_class_name('name').text,
+                'summary': driver.find_element_by_class_name('basic-info-description').text,
+                'logo': driver.find_element_by_class_name('image').get_attribute('src'),
+                'website': driver.find_element_by_class_name('website').find_element_by_css_selector('p').text,
+                'industry': driver.find_element_by_class_name('industry').text,
+                'address': driver.find_element_by_class_name('adr').text,
+                'specialties': driver.find_element_by_class_name('specialties').find_element_by_css_selector('p').text,
+                'company size': driver.find_element_by_class_name('company-size').text,
+                'type': driver.find_element_by_class_name('type').find_element_by_css_selector('p').text
+            }
+            time.sleep(5)
+            driver.quit()
+        except Exception:
+            return 'Oops! Something went wrong, please check the url', 400
+        return results, 200
