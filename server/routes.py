03b3bb90986704002261b1d4a005f1a1333ff46f
Hamid Zade <hamidmamadzade@gmail.com>
2016-02-16 16:03:32 +0000

    refactors routes

--- server/routes.py
+++ server/routes.py
@@ -8,68 +8,24 @@ from server.controllers.sessions import SessionsAPI
 from server.controllers.external_resources import CompanyScraperAPI, PeopleScraperAPI, GetJobsAPI
 # from server.controllers.company_scraper import CompanyScraperAPI
 
-api.add_resource(
-    CompanyScraperAPI,
-    '/api/companyscraper',
-    endpoint='companyscraper'
-)
+api.add_resource(CompanyScraperAPI, '/web/companies')
 
-api.add_resource(
-    PeopleScraperAPI,
-    '/api/peoplescraper',
-    endpoint='peoplescraper'
-)
+api.add_resource(PeopleScraperAPI, '/web/people')
 
-api.add_resource(
-    CompaniesAPI,
-    '/api/companies',
-    endpoint='companies'
-)
+api.add_resource(GetJobsAPI, '/web/jobs')
 
-api.add_resource(
-    CompanyAPI,
-    '/api/companies/<int:id>',
-    endpoint='company'
-)
+api.add_resource(CompaniesAPI, '/api/companies')
 
-api.add_resource(
-    JobsAPI,
-    '/api/companies/<int:company_id>/jobs',
-    endpoint='jobs'
-)
+api.add_resource(CompanyAPI, '/api/companies/<int:id>')
 
-api.add_resource(
-    JobAPI,
-    '/api/companies/<int:company_id>/jobs/<int:id>',
-    endpoint='job'
-)
+api.add_resource(JobsAPI, '/api/companies/<int:company_id>/jobs')
 
-api.add_resource(
-    PeopleAPI,
-    '/api/companies/<int:company_id>/people',
-    endpoint='people'
-)
+api.add_resource(JobAPI, '/api/companies/<int:company_id>/jobs/<int:id>')
 
-api.add_resource(
-    PersonAPI,
-    '/api/companies/<int:company_id>/people/<int:id>',
-    endpoint='person'
-)
+api.add_resource(PeopleAPI, '/api/companies/<int:company_id>/people')
 
-api.add_resource(
-    GetJobsAPI,
-    '/api/getjobs',
-    endpoint='getjobs'
-)
+api.add_resource(PersonAPI, '/api/companies/<int:company_id>/people/<int:id>')
 
-api.add_resource(
-    UsersAPI,
-    '/api/users',
-    endpoint='users'
-)
+api.add_resource(UsersAPI, '/api/users')
 
-api.add_resource(
-    SessionsAPI,
-    '/api/sessions',
-    endpoint='sessions'
-)
+api.add_resource(SessionsAPI, '/api/sessions')
