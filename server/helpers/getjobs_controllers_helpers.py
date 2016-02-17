0a1896b87cb3499edea9229806a640a91f4c4c01
Andrew Burnie <apburnie@hotmail.co.uk>
2016-02-17 10:34:17 +0000

    enables jobs queries with whitespaces

--- server/helpers/getjobs_controllers_helpers.py
+++ server/helpers/getjobs_controllers_helpers.py
@@ -1,10 +1,13 @@
 import urllib.request
+import shlex
 import xml.etree.ElementTree as ET
 
+
 def get_xml(key, query, location):
+    formatted_query = '-'.join(shlex.split(query))
     xml_file = urllib.request.urlopen('http://api.indeed.com/ads/apisearch?publisher='
       + key + '&q='
-      + query + '&l=' + location
+      + formatted_query + '&l=' + location
       +'&st=employer&latlong=1&co=gb&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2')
     return ET.parse(xml_file).getroot().findall('results/result')
 
