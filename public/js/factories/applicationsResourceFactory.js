636c97f10e866744e977839a4d7b4f1491a24535
michaellennox <michaelctlennox@gmail.com>
2016-02-18 15:07:55 +0000

    Fixes applications resource factory

--- public/js/factories/applicationsResourceFactory.js
+++ public/js/factories/applicationsResourceFactory.js
@@ -2,11 +2,11 @@ jobber.factory('applicationsResourceFactory', ['$http', function($http) {
   var self = {};
 
   self.getApplications = function() {
-    return $http.get('/api/companies');
+    return $http.get('/api/applications');
   };
 
   self.postApplications = function(company_id) {
-    return $http.post('/api/companies', {company_id: company_id});
+    return $http.post('/api/applications', {company_id: company_id});
   };
 
   return self;
