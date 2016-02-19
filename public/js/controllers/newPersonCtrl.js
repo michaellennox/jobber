18ef02b401c9709f9742f073b9240c6bf17ec4a3
michaellennox <michaelctlennox@gmail.com>
2016-02-19 11:44:41 +0000

    People scraper works

--- public/js/controllers/newPersonCtrl.js
+++ public/js/controllers/newPersonCtrl.js
@@ -8,4 +8,16 @@ jobber.controller('NewPersonCtrl', ['peopleResourceFactory', '$window', '$stateP
         $window.location.href = '/companies/' + self.company_id;
       });
   };
+
+  self.scrapePersonData = function() {
+    peopleResourceFactory.getPersonFromLinkedin(self.personURL)
+      .then(function(response){
+        self.scrapedData = response.data;
+        self.name = response.data.name;
+        self.job_title = response.data.job_title;
+        self.location = response.data.location;
+        self.photo = response.data.photo;
+        self.summary = response.data.summary;
+      });
+  };
 }]);
