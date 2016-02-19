68c507698b093ca9ef8746c615b1f6dc04e2f033
Hamid Zade <hamidmamadzade@gmail.com>
2016-02-19 12:58:09 +0000

    updates company views

--- public/js/controllers/viewCompanyCtrl.js
+++ public/js/controllers/viewCompanyCtrl.js
@@ -8,6 +8,13 @@ jobber.controller('ViewCompanyCtrl', ['companiesResourceFactory', '$stateParams'
         self.company = response.data;
         self.people = response.data.people;
         self.jobs = response.data.jobs;
+        self.summary = response.data.summary;
+        self.website = response.data.website;
+        self.industry = response.data.industry;
+        self.city = response.data.city;
+        self.postcode = response.data.postcode;
+        self.size = response.data.size;
+        self.logo = response.data.logo;
       });
   })();
 
